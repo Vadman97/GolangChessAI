973433044a7048ad1bc6143f3ac9ab8c90b94b84
Vadim Korolik <vkorolik@gmail.com>
2019-03-25 02:09:54 -0700

    fix metrics formatting

--- pkg/chessai/player/ai/metrics.go
+++ pkg/chessai/player/ai/metrics.go
@@ -12,7 +12,7 @@ func (metrics *Metrics) Print() (res string) {
 	res += fmt.Sprintf("Considered %d\n", metrics.MovesConsidered)
 	pruned := metrics.MovesPrunedAB + metrics.MovesPrunedTransposition
 	prunedPercent := 100 * float64(pruned) / float64(pruned+metrics.MovesConsidered)
-	res += fmt.Sprintf("\tPruned     %f (%d)\n", prunedPercent, pruned)
+	res += fmt.Sprintf("\tPruned     %f%% (%d)\n", prunedPercent, pruned)
 	if metrics.MovesPrunedAB > 0 || metrics.MovesPrunedTransposition > 0 {
 		res += fmt.Sprintf("\t\tPrunedAB:    %d\n", metrics.MovesPrunedAB)
 		res += fmt.Sprintf("\t\tPrunedTrans: %d\n", metrics.MovesPrunedTransposition)
