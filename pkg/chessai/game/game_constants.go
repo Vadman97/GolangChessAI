fb0c62106f6936a40cf47b3e436b27a5a963831d
Kishan Trivedi <kishantrivedi73@gmail.com>
2019-04-12 17:19:11 -0700

    Added statuses for different types of stalemates  (#74)
    * Added statuses for different types of stalemates as well as for the not yet implemented 3 move stalemate
    
    * Changed status names as per Devans request
    
    * Changed regular to stalemate

--- pkg/chessai/game/game_constants.go
+++ pkg/chessai/game/game_constants.go
@@ -1,11 +1,13 @@
 package game
 
 const (
-	Active    = byte(iota)
-	WhiteWin  = byte(iota)
-	BlackWin  = byte(iota)
-	Stalemate = byte(iota)
-	Aborted   = byte(iota)
+	Active                      = byte(iota)
+	WhiteWin                    = byte(iota)
+	BlackWin                    = byte(iota)
+	Stalemate                   = byte(iota)
+	FiftyMoveDraw               = byte(iota)
+	RepeatedActionThreeTimeDraw = byte(iota)
+	Aborted                     = byte(iota)
 )
 
 var StatusStrings = [...]string{
@@ -13,5 +15,7 @@ var StatusStrings = [...]string{
 	"White Win",
 	"Black Win",
 	"Stalemate",
+	"Fifty Move Draw",
+	"Repeated Action Three Times Draw",
 	"Aborted",
 }
